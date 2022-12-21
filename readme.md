019bbc585fa3b30caadd047c1429f8cf8d1364a4
MORIN Thomas <thomas.morin@inria.fr>
2022-12-21 14:42:27 +0100

    tiling + readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+# TIME CLASSICAL
+
+PC perso : gcc 9.4.0 
+-O0 : 8min37
+-O1 : 5min40
+-O2 : 5min32
+-O3 : 5m30
+-Ofast
+
+
+en inversant le sens des boucles, on prend que 0.52 min
+pas d'influence du restrict
+
+TILING : 34 secs 
--- stencil9.c
+++ stencil9.c
@@ -8,6 +8,35 @@
 #define SIZEX 10000
 #define SIZEY 10000
 
+#define TILEX 100
+#define TILEY 100
+
+void stencil9_tiled(float * restrict a, const float * restrict b, const int tile_x, const int tile_y) {
+  int i,j;
+  int sizeij = 2*SIZEY+2, sizeip1j, sizeip2j, sizeim1j, sizeim2j, sizeijp1, sizeijm1, sizeijm2, sizeijp2 ;
+  sizeip2j = sizeip1j+SIZEY ;
+  sizeim1j = sizeij - SIZEY ;
+  sizeim2j = sizeim1j - SIZEY ;
+  sizeijp1 = sizeij+1 ;
+  sizeijp2 = sizeijp1+1;
+  sizeijm1 = sizeij-1 ;
+  sizeijm2 = sizeijm1-1 ;
+  int nb_X = SIZEX / tile_x, nb_Y = SIZEY / tile_y ;
+  int tx, ty ;
+  for (tx = 0 ; tx < nb_X ; tx++) {
+      for (ty = 0 ; ty < nb_Y ; ty++) {
+		for (i = (tx == 0)*2; i < tile_x - (tx == (nb_X-1))*2; i++) {
+			for (j = (ty == 0)*2; j < tile_y - (ty == (nb_Y-1))*2; j++) {
+				sizeij = (tx*tile_x + i)*SIZEX + ty*tile_y + j ;
+    				a[sizeij] = (8*b[sizeij] + b[sizeij + SIZEY] + b[sizeij + 2*SIZEY] 
+					+ b[sizeij - SIZEY] + b[sizeij - 2*SIZEY] + b[sizeij+1]
+					+ b[sizeij - 1] + b[sizeij - 2] + b[sizeij + 2])/9.;
+			}
+		}
+	    }
+     }
+}
+
 void stencil9(float * restrict a, const float * restrict b) {
   int i,j;
   int sizeij = 2*SIZEY+2, sizeip1j, sizeip2j, sizeim1j, sizeim2j, sizeijp1, sizeijm1, sizeijm2, sizeijp2 ;
@@ -32,10 +61,11 @@ void stencil9(float * restrict a, const float * restrict b) {
 	          a[sizeij] = (8*b[sizeij] + b[sizeip1j] + b[sizeip2j] 
 			+ b[sizeim1j] + b[sizeim2j] + b[sizeijp1]
 			+ b[sizeijm1] + b[sizeijm2] + b[sizeijp2])/9.;
-
       }
     }
 }
+
+
 float dot1D(float *a,float *b,int n)  {
   int i;
   float s=0;
@@ -66,9 +96,9 @@ float dot2D(float *a,float *b)  {
   /* Iterative computation. Iterate while error greater than ERROR */
     for(k=0;k<100;k++) {
     float *c;
-    stencil9(a,b);
+    stencil9_tiled(a,b, TILEX, TILEY);
     s+=dot2D(a,b);
-    stencil9(b,a);
+    stencil9_tiled(b,a, TILEX, TILEY);
     s+=dot2D(a,b);
     fprintf(stderr,".");
   }
